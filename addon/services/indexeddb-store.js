de7ea4e79fc891bb67adbc91d0ae56d984b90df0
Martin Feckie <mfeckie@gmail.com>
2015-03-29 10:27:07 +0800

    Remove promise from _addObjectStore as init does not respect promises

--- addon/services/indexeddb-store.js
+++ addon/services/indexeddb-store.js
@@ -121,31 +121,26 @@ export default Ember.Service.extend({
   },
   _addObjectStores: function () {
     var self = this;
-    return new Ember.RSVP.Promise(function(resolve, reject){
-      var version = self.get('version');
+    var version = self.get('version');
 
-      var openRequest = indexedDB.open(self.get('databaseNamespace'), version);
+    var openRequest = indexedDB.open(self.get('databaseNamespace'), version);
 
 
-      openRequest.onupgradeneeded = function (e) {
-        var db = e.target.result;
-        self.objectStores.forEach(function(storeName){
-          if (!db.objectStoreNames.contains(storeName)) {
-            db.createObjectStore(storeName, { autoIncrement : true });
-          }
-        });
-      };
+    openRequest.onupgradeneeded = function (e) {
+      var db = e.target.result;
+      self.objectStores.forEach(function(storeName){
+        if (!db.objectStoreNames.contains(storeName)) {
+          db.createObjectStore(storeName, { autoIncrement : true });
+        }
+      });
+    };
 
-      openRequest.onsuccess = function (e) {
-        e.target.result.close();
-        resolve();
-      };
+    openRequest.onsuccess = function (e) {
+      e.target.result.close();
+    };
 
-      openRequest.onerror = function (e) {
-        console.error(e);
-        reject();
-      };
-
-    });
+    openRequest.onerror = function (e) {
+      console.error(e);
+    };
   }
 });
